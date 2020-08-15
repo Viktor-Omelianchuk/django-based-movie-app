153e64102b2549a88f0f5b39021a3c1777ac4208
Viktor Omelianchuk <omelianchuk.viktor@gmail.com>
2020-08-15 19:55:28 +0300

    update review form

---
+++ movies/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import Reviews
+
+class ReviewForm(forms.ModelForm):
+    """Feedback form"""
+    class Meta:
+        model = Reviews
+        fields = ('name', 'email', 'text')
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
-
+from .forms import ReviewForm
 from .models import Movie
 
 
@@ -9,10 +9,21 @@ class MovieView(ListView):
     """List of films"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
-    # template_name = "movies/movies.html"
+
 
 
 class MovieDetailView(DetailView):
     """Full description of the film"""
     model = Movie
     slug_field = 'url'
+
+class AddReview(View):
+    """Reviews"""
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
