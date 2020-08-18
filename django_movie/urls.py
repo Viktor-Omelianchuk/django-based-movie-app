64aff42273c63f41cbec8cc6e55226463f49cccb
Viktor Omelianchuk <omelianchuk.viktor@gmail.com>
2020-08-18 22:08:59 +0300

    added email subscription form

--- django_movie/urls.py
+++ django_movie/urls.py
@@ -21,9 +21,9 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('', include('movies.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
-
+    path('contact/', include("contact.urls")),
+    path('', include('movies.urls')),
 ]
 
 if settings.DEBUG:
