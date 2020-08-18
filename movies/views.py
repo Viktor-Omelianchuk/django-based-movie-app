5841f4f0458c3d0b454f4a435a66a1c64ca1a62b
Viktor Omelianchuk <omelianchuk.viktor@gmail.com>
2020-08-18 16:58:36 +0300

    added movie search

--- movies/views.py
+++ movies/views.py
@@ -107,3 +107,16 @@ class FilterMoviesView(GenreYear, ListView):
         context["year"] = ''.join([f"year={x}&" for x in self.request.GET.getlist("year")])
         context["genre"] = ''.join([f"genre={x}&" for x in self.request.GET.getlist("genre")])
         return context
+
+
+class Search(ListView):
+    """Movie search"""
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Movie.objects.filter(title__icontains=self.request.GET.get('q'))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['q'] = f'q={self.request.GET.get("q")}&'
+        return context
